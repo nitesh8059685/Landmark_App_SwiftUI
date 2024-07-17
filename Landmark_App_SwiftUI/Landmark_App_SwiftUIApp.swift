a432bdc0d580ca6d3805e3079a9527837981621f
nitesh8059685@ <nitesh8059685@gmail.com>
2024-07-17 18:25:49 +0530

    Initial Commit

---
+++ Landmark_App_SwiftUI/Landmark_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmark_App_SwiftUIApp.swift
+//  Landmark_App_SwiftUI
+//
+//  Created by Nitesh Sharma on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Landmark_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Landmark_App_SwiftUIUITests/Landmark_App_SwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Landmark_App_SwiftUIUITestsLaunchTests.swift
+//  Landmark_App_SwiftUIUITests
+//
+//  Created by Nitesh Sharma on 17/07/24.
+//
+
+import XCTest
+
+final class Landmark_App_SwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
