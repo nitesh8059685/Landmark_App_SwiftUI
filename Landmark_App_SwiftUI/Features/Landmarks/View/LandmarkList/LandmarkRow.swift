c6a1a47e4ce93fde43955d8583e9113a187024d7
nitesh8059685@ <nitesh8059685@gmail.com>
2024-07-19 16:45:20 +0530

    Landmark List view with Navigation to Landmark Details

---
+++ Landmark_App_SwiftUI/Features/Landmarks/View/LandmarkList/LandmarkRow.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkRow.swift
+//  Landmark_App_SwiftUI
+//
+//  Created by Nitesh Sharma on 19/07/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow()
+}
